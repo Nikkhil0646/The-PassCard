def21d9172f546ccaec41ecba6b8ca13f78d66f9
Gauravvpnd <gouravpanda2k04@gmail.com~>
2023-08-20 19:13:52 +0530

    changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,11 @@ import '../components/Header.css'
 
 const Header = () => {
      
-   const handle  = (e) => {
-     window.location.reload();
-     e.preventDefault()
-   }
+  
  
   return (
     <div className='auth'>
-       <h1 className='me' onClick={handle}>The PassCard</h1>
+       <h1 className='me'>The PassCard</h1>
        
     </div>
    
