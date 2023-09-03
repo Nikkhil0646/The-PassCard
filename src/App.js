a90a48ed64d3f39ba194c2f21aee9ae73d345ed7
gauravvpnd <gouravpanda2k04@gmail.com~>
2023-09-03 17:51:04 +0530

    resources

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 // import Contact from './components/Contact';
-import Link from './Links/Link';
+
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <Header />
     <Body />
     {/* <Contact /> */}
-    <Link />
+    
    </div>
     
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import SignIn from './SignIn';
+import Link from './Links/Link';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <Routes>
       <Route path="/" element={<SignIn />} />
       <Route path="/app" element={<App />} />
+      <Route path="/link" element={<Link />} />
     </Routes>
   </Router>
 );
